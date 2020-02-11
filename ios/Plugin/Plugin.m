2809a20047bde7c93f2482ebaa5941ecd4a4a291
Marc Pascual <marc.pascual@bitgenoma.com>
2020-02-11 16:20:37 +0100

    Add ios platform

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,14 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(AdMobAds, "AdMobAds",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(setOptions, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(createBannerView, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(showBannerAd, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(destroyBannerView, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(createInterstitialView, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(requestInterstitialAd, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(showInterstitialAd, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(createRewardedView, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(requestRewardedAd, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(showRewardedAd, CAPPluginReturnPromise);
 )
